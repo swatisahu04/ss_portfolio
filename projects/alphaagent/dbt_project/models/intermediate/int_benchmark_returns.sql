0a4d9096e0b012e87ace8144e19df0676656ab76
Swati <44176030+swatisahu04@users.noreply.github.com>
2026-04-18 18:34:10 -0400

    Delete projects/alphaagent directory

--- projects/alphaagent/dbt_project/models/intermediate/int_benchmark_returns.sql
+++
@@ -1,7 +0,0 @@
-{{ config(tags=['intermediate', 'benchmarks']) }}
-
-select
-    benchmark_id,
-    price_date,
-    daily_return
-from {{ ref('stg_benchmarks_daily') }}
--- projects/alphaagent/dbt_project/models/staging/stg_positions_daily.sql
+++
@@ -1,8 +0,0 @@
-{{ config(tags=['staging', 'positions']) }}
-
-select
-    portfolio_id,
-    security_id,
-    position_date,
-    quantity
-from {{ source('raw', 'positions_daily') }}
